20380a705a9d15e647316e069b26b44bf411d1fb
Juuso Solonen <m6752@student.jamk.fi>
2019-09-29 02:07:56 +0300

    unicafe step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,28 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const Jutska = (props) => {
-   console.log("props:", props)
-    return(
-        <>
-         <h1>{props.header}</h1><br/>
-         <p>{props.para}</p>
-         </>
-    )
-}
+const Header = () => <h1>give feedback</h1>
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+        {props.text}
+    </button>
+)
+const Stats = () => <h1>statistics</h1>
+
+const Smth = (props) => (
+    <>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+    </>
+)
 
 const App = () => {
-    const juttu = {
-    header: "I have a huge penis",
-    para: "This was made with ReactJS"
-    }
-    
-    return (
-    <>
-    <Jutska header={juttu.header} para={juttu.para} />
-    </>
-    )
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Stats />
+      <Smth good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
 }
-ReactDOM.render(<App />, document.getElementById('root'));
 
-
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
