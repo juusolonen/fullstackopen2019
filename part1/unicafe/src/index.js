764784255cf6dead222dc57a6fd18d22c5d70802
Juuso Solonen <m6752@student.jamk.fi>
2019-09-29 03:01:46 +0300

    unicafe step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import { tsPropertySignature } from '@babel/types'
 
 const Header = () => <h1>give feedback</h1>
 const Button = (props) => (
@@ -9,20 +10,42 @@ const Button = (props) => (
 )
 const Stats = () => <h1>statistics</h1>
 
-const Smth = (props) => (
-    <>
-    <p>good {props.good}</p>
-    <p>neutral {props.neutral}</p>
-    <p>bad {props.bad}</p>
+const Statistic = (props) => {
+    return (
+        <>
+        <p>{props.text} {props.value}</p> 
+        </>
+    )
+}
+
+const Statistics = (props) => {
+    if(props.all === 0) {
+        return (
+            <>
+            <p>No feedback given</p>
+            </>
+        )
+    }
+    return(
+        <>
+    <Statistic text="good" value={props.good} />
+    <Statistic text="neutral" value={props.neutral} />
+    <Statistic text="bad" value={props.bad} />
+    <Statistic text="all" value={props.all} />
+    <Statistic text="average" value={props.average} />
+    <Statistic text="positive" value={props.positive} />
     </>
-)
+    )
+}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const all = good + bad + neutral
+  const average = (good + (0 * neutral) + ((-1) * bad)) / (good + neutral + bad)
+  const positive = (good / (good + neutral + bad))*100 + " %"
   return (
     <div>
       <Header />
@@ -30,7 +53,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Stats />
-      <Smth good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/>
     </div>
   )
 }
