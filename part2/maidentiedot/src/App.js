de88f97e8d1cda979cc5bfff2e4485a5b136d67f
Juuso Solonen <m6752@student.jamk.fi>
2020-05-06 19:45:16 +0300

    maiden tiedot step1

---
+++ part2/maidentiedot/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Axios from 'axios'
+import Filter from './components/Filter'
+import Result from './components/Result'
+
+function App() {
+  const [ filter, setFilter ] = useState('')
+  const [ countries, setCountries ] = useState([])
+  const [ found, setFound ] = useState(null)
+ 
+  useEffect(()=> {
+    Axios.get('https://restcountries.eu/rest/v2/all')
+    .then(response => {
+      setCountries(response.data)
+    })
+  }, [filter])
+
+  return (
+    <div >
+      <Filter filter={filter} setFilter={setFilter} countries={countries} found={found} setFound={setFound}/>
+      <Result found={found} />
+    </div>
+  );
+}
+
+export default App;
---
+++ part2/maidentiedot/src/components/Result.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Found = ({found}) => {
+
+    if(found) {
+
+        if(found.length > 10){
+                return <p>Too many matches, specify another filter</p>
+            } 
+
+        else if (found.length <10 && found.length > 1){
+                return(
+                        found.map(country => 
+                        <p key={country.name}>{country.name}</p>)
+                    )
+            } 
+
+        else if (found.length == 1) {
+                return (
+                    <div>
+                    <div>
+                    <h1>{found[0].name}</h1>
+                    <p>capital {found[0].capital}</p>
+                    <p>population {found[0].population}</p>
+                    </div>
+                <div>
+                    <h2>languages</h2>
+                        <ul>
+                        {found[0].languages.map(language => <li key={language.name}>{language.name}</li>)}
+                        </ul>          
+                </div>
+                <div>
+                    <img src={found[0].flag} width="50px"/>
+                </div>
+            </div>
+                )
+            }
+    }
+
+    return null
+
+}
+
+export default Found
