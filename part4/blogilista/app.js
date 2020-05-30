65e1e3ff4993b61c63f055471038642d8dcde3dc
Juuso Solonen <m6752@student.jamk.fi>
2020-05-30 11:02:58 +0300

    blogilista step1

---
+++ part4/blogilista/app.js
@@ -0,0 +1,51 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+
+
+const mongoUrl = 'mongodb+srv://juusokayttaja2:kayttaja123@cluster0-ljqfc.mongodb.net/puhelinluettelo?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(result => {
+    console.log("Yhteys luotu")
+  })
+  .catch(err => console.log(err))
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
