6a8390cfcd53f0a7e6602c073d1b5be873f6e448
Juuso Solonen <m6752@student.jamk.fi>
2020-05-30 15:32:15 +0300

    blogilista step2 controllers/blogs

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -5,11 +5,10 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
 
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(result => {
     console.log("Yhteys luotu")
   })
@@ -19,25 +18,9 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
