c223c04107573702ab6aabbb2fee9e209e972c9a
Juuso Solonen <m6752@student.jamk.fi>
2020-06-03 18:54:55 +0300

    blogilistan testit step2

--- part4/blogilista/tests/blog_api.test.js
+++ part4/blogilista/tests/blog_api.test.js
@@ -17,6 +17,12 @@ test('blogs are returned as json', async () => {
       .expect('Content-type', /application\/json/)
 })
 
+test('blog identifying field is called id', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+})
+
 
 afterAll(() => {
     mongoose.connection.close()
