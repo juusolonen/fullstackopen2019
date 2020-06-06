0d65e7cc421235488fb5d3b72b88c40cc10aa96e
Juuso Solonen <m6752@student.jamk.fi>
2020-06-06 21:50:14 +0300

    blogilistan laajennus step6

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+    .find({}).populate('user', {username: 1, name: 1, id: 1})
         if (blogs) {
           response.json(blogs)
         } else {
@@ -12,7 +14,16 @@ blogsRouter.get('/', async (request, response) => {
   })
 
   blogsRouter.post('/', async (request, response, next) => {
-    const blog = new Blog(request.body)
+
+    const user = await User.findOne()
+
+    const blog = new Blog({
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes,
+      user: user._id
+    })
     if (!blog.likes) {blog.likes = 0}
     if (!blog.url || !blog.title) {
       response.status(400).end()
@@ -20,6 +31,8 @@ blogsRouter.get('/', async (request, response) => {
   
     try {
     const addedBlog = await blog.save()
+    user.blogs = user.blogs.concat(addedBlog._id)
+    await user.save()
     response.status(201).json(addedBlog)
     } catch (excp) {
       next(excp)
